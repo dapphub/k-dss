b49563f0fa2c0ae1cbb9d47bf8af42856995f21d
Lev Livnev <lev@symbolic.partners>
2019-07-04 10:39:56 +0200

    shell.nix: add warning when KLAB_PATH not set

--- shell.nix
+++ shell.nix
@@ -15,6 +15,7 @@ stdenv.mkDerivation {
     z3
   ];
   shellHook = ''
+    if [ -z "$KLAB_PATH" ]; then echo "WARNING: The environment variable KLAB_PATH should be set to point to the klab repo. Please fix and reënter the nix shell."; fi
     export PATH=$KLAB_PATH/node_modules/.bin/:$KLAB_PATH/bin:$PATH
     export KLAB_EVMS_PATH=$KLAB_PATH/evm-semantics
   '';
