a8e19f31df3d1458c11195e54cb1c23e7bd5bd81
Mariano Conti <nanexcool@gmail.com>
2020-06-08 14:03:56 -0300

    Use latest z3

--- shell.nix
+++ shell.nix
@@ -1,10 +1,5 @@
 with import <nixpkgs> {};
-let
-  pkgs-6ec8fe0 = import (fetchGit {
-    url = "https://github.com/NixOS/nixpkgs";
-    rev = "6ec8fe0408d8940dcbeea6b01cab071062fd8f2d";
-  }) {};
-in
+
 stdenv.mkDerivation {
   name = "k-dss";
   buildInputs = [
@@ -21,7 +16,7 @@ stdenv.mkDerivation {
     wget
     zip
     zstd
-    pkgs-6ec8fe0.z3
+    z3
   ];
   shellHook = ''
     if [ -z "$KLAB_PATH" ]; then echo "WARNING: The environment variable KLAB_PATH should be set to point to the klab repo. Please fix and reënter the nix shell."; fi
