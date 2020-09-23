7a63f431aaf91152b50365564d6410582e62a2ec
Everett Hildenbrandt <everett.hildenbrandt@gmail.com>
2020-09-23 04:57:01 +0000

    Makefile, write-gas.py: use pyk library for writing gas expressions

--- write-gas.py
+++ write-gas.py
@@ -2,30 +2,15 @@
 
 import json
 import sys
+import pyk
 
 input_file = sys.argv[1]
 
 with open(input_file) as f:
     input_json = json.load(f)
 
-def printIt(input):
-    if input['node'] == 'KToken':
-        return input['token']
-    if input['node'] == 'KVariable':
-        return input['originalName']
-    if input['node'] == 'KApply':
-        argStrs = [ printIt(arg) for arg in input['args'] ]
-        if input['label'] in [ '_+Int_' , '_-Int_' , '_/Int_' , '_*Int_' , '_orBool_' , '_andBool_' ]:
-            return '(' + argStrs[0] + ' ' + input['label'][1:-1] + ' ' + argStrs[1] + ')'
-        elif input['label'] == '_==K_':
-            return '(' + argStrs[0] + ' ==Int ' + argStrs[1] + ')'
-        elif input['label'] == '_=/=K_':
-            return '(' + argStrs[0] + ' =/=Int ' + argStrs[1] + ')'
-        elif input['label'] == 'notBool_':
-            return '(notBool ' + argStrs[0] + ')'
-        elif input['label'] == '#if_#then_#else_#fi_K-EQUAL-SYNTAX':
-            return '#if ' + argStrs[0] + ' #then ' + argStrs[1] + ' #else ' + argStrs[2] + ' #fi'
-        else:
-            return input['label'] + '(' + ', '.join(argStrs) + ')'
+definition  = pyk.readKastTerm('deps/evm-semantics/.build/defn/java/driver-kompiled/compiled.json')
+symbolTable = pyk.buildSymbolTable(definition)
+symbolTable['#gas'] = appliedLabelStr('#gas')
 
-print(printIt(input_json))
+print(pyk.prettyPrintKast(input_json, symbolTable))
