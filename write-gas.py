e0fff405dec92d286576390cd176b95538125eda
Everett Hildenbrandt <everett.hildenbrandt@gmail.com>
2020-10-05 18:28:35 +0000

    write-gas.py: simplify gas expressions directly

--- write-gas.py
+++ write-gas.py
@@ -10,19 +10,21 @@ input_file = sys.argv[1]
 
 definition = pyk.readKastTerm('deps/evm-semantics/.build/defn/java/driver-kompiled/compiled.json')
 
+ite_label     = '#if_#then_#else_#fi_K-EQUAL-SYNTAX'
+inf_gas_label = 'infGas'
+
+symbolTable = pyk.buildSymbolTable(definition)
+symbolTable[inf_gas_label] = pyk.appliedLabelStr('#gas')
+symbolTable['notBool_']    = pyk.paren(pyk.underbarUnparsing('notBool_'))
+symbolTable[ite_label]     = lambda c, b1, b2: '#if ' + c + '\n  #then ' + pyk.indent(b1) + '\n  #else ' + pyk.indent(b2) + '\n#fi'
+for label in ['+Int', '-Int', '*Int', '/Int', 'andBool', 'orBool']:
+    symbolTable['_' + label + '_'] = pyk.paren(pyk.binOpStr(label))
+
 with open(input_file) as f:
     input_json = json.load(f)
 
 gas_exp    = input_json['args'][0]
 constraint = input_json['args'][1:]
-
-ite_label = '#if_#then_#else_#fi_K-EQUAL-SYNTAX'
-
-symbolTable = pyk.buildSymbolTable(definition)
-symbolTable['infGas']   = pyk.appliedLabelStr('#gas')
-symbolTable['notBool_'] = pyk.paren(pyk.underbarUnparsing('notBool_'))
-for label in ['+Int', '-Int', '*Int', '/Int', 'andBool', 'orBool']:
-    symbolTable['_' + label + '_'] = pyk.paren(pyk.binOpStr(label))
 
 def gatherConstInts(input, constants = [], non_constants = []):
     if pyk.isKApply(input) and input['label'] == '_+Int_':
@@ -52,16 +54,23 @@ def simplifyPlusInt(k):
         return buildPlusInt(vs)
     return k
 
-simplified_json = pyk.simplifyBool(gas_exp)
+simplified_json = pyk.simplifyBool(input_json)
 simplified_json = simplifyPlusInt(simplified_json)
 
 rewrites = [ (KApply('_==K_', [KVariable('I1'), KVariable('I2')]),  KApply('_==Int_', [KVariable('I1'), KVariable('I2')]))
            , (KApply('_=/=K_', [KVariable('I1'), KVariable('I2')]), KApply('_=/=Int_', [KVariable('I1'), KVariable('I2')]))
+           , ( KApply(ite_label, [KVariable('COND'), KApply(inf_gas_label, [KVariable('G1')]), KApply(inf_gas_label, [KVariable('G2')])])
+             , KApply(inf_gas_label, [KApply(ite_label, [KVariable('COND'), KVariable('G1'), KVariable('G2')])])
+             )
            , ( KApply(ite_label, [KVariable('C'), KApply('_+Int_', [KVariable('I'), KVariable('I1')]), KApply('_+Int_', [KVariable('I'), KVariable('I2')])])
              , KApply('_+Int_', [KVariable('I'), KApply(ite_label, [KVariable('C'), KVariable('I1'), KVariable('I2')])])
              )
            , ( KApply(ite_label, [KVariable('C'), KApply('_+Int_', [KVariable('I1'), KVariable('I')]), KApply('_+Int_', [KVariable('I2'), KVariable('I')])])
              , KApply('_+Int_', [KVariable('I'), KApply(ite_label, [KVariable('C'), KVariable('I1'), KVariable('I2')])])
+             )
+           # To remove side-conditions nested in #ite for now, should be dealt with as substitutions instead.
+           , ( KApply('#And', [KApply(inf_gas_label, [KVariable('G')]), KVariable('SUBST')])
+             , KApply(inf_gas_label, [KVariable('G')])
              )
            ]
 
