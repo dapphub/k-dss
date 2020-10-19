4bc3fab39c5616e38448dca53091292479a14b47
Everett Hildenbrandt <everett.hildenbrandt@gmail.com>
2020-10-19 23:10:54 +0000

    check-subproofs: take hash directly and handle zero trusted rules

--- check-subproofs.sh
+++ check-subproofs.sh
@@ -3,15 +3,17 @@
 set -euo pipefail
 
 proof_name="$1"
-proof_hash="$(make $proof_name.hash | tail -n1)"
+proof_hash="$2"
 proof_hash_upper="$(echo $proof_hash | tr '[:lower:]' '[:upper:]')"
 
 echo "Proof name: $proof_name"
 echo "Proof hash: $proof_hash"
 
-count_trusted_rules="$(grep -c 'trusted' out/specs/$proof_hash.k)"
+count_trusted_rules="$(grep -c 'trusted' out/specs/$proof_hash.k)" || count_trusted_rules='0'
 echo "Number of trusted rules:"
 echo "$count_trusted_rules"
+
+[[ "$count_trusted_rules" != 0 ]] || exit 0
 
 found_rules=()
 for rule in $(grep 'SRULE' out/data/$proof_hash.log | cut --delimiter=' ' --field=3 | cut --delimiter='_' --field=1); do
