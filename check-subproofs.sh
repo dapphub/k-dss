d37c4a47963c623636695832e9a802d8dd6ce6ea
Everett Hildenbrandt <everett.hildenbrandt@gmail.com>
2020-10-19 22:46:03 +0000

    check-subproofs.sh: script to check that correct subproofs arre applied

---
+++ check-subproofs.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+proof_name="$1"
+proof_hash="$(make $proof_name.hash | tail -n1)"
+proof_hash_upper="$(echo $proof_hash | tr '[:lower:]' '[:upper:]')"
+
+echo "Proof name: $proof_name"
+echo "Proof hash: $proof_hash"
+
+count_trusted_rules="$(grep -c 'trusted' out/specs/$proof_hash.k)"
+echo "Number of trusted rules:"
+echo "$count_trusted_rules"
+
+found_rules=()
+for rule in $(grep 'SRULE' out/data/$proof_hash.log | cut --delimiter=' ' --field=3 | cut --delimiter='_' --field=1); do
+    rule_name=$(cat "out/data/${proof_hash}_blobs/$rule.json"                       \
+                    | jq .term.att                                                  \
+                    | grep --only-matching "label($proof_hash_upper.[A-Za-z.]*)"    \
+                    | cut --delimiter='(' --field=2 | cut --delimiter=')' --field=1 \
+                    | grep --only-matching '\..*'
+               )
+    found=false
+    for r in "${found_rules[@]}"; do [[ "$r" != "$rule_name" ]] || found=true; done
+    $found || found_rules+=("${rule_name:1}")
+done
+
+count_found_rules="${#found_rules[@]}"
+
+echo "Proof SRULE names:"
+echo "${found_rules[@]}"
+
+[[ "$count_trusted_rules" == "$count_found_rules" ]]
