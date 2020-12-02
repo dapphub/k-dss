f388017c6991d593e0992f5d4a8c4e9c30819f9a
Everett Hildenbrandt <everett.hildenbrandt@gmail.com>
2020-12-02 15:24:21 +0000

    check-subproofs: correct duplicate applications of same proof

--- check-subproofs.sh
+++ check-subproofs.sh
@@ -15,18 +15,16 @@ echo "$count_trusted_rules"
 
 [[ "$count_trusted_rules" != 0 ]] || exit 0
 
-found_rules=()
-for rule in $(grep 'SRULE' out/data/$proof_hash.log | cut --delimiter=' ' --field=3 | cut --delimiter='_' --field=1); do
-    rule_name=$(cat "out/data/${proof_hash}_blobs/$rule.json"                       \
-                    | jq .term.att                                                  \
-                    | grep --only-matching "label($proof_hash_upper.[A-Za-z.]*)"    \
-                    | cut --delimiter='(' --field=2 | cut --delimiter=')' --field=1 \
-                    | grep --only-matching '\..*'
-               )
-    found=false
-    for r in "${found_rules[@]}"; do [[ "$r" != "$rule_name" ]] || found=true; done
-    $found || found_rules+=("${rule_name:1}")
-done
+find_rules() {
+    for rule in $(grep 'SRULE' out/data/$proof_hash.log | cut --delimiter=' ' --field=3 | cut --delimiter='_' --field=1); do
+        cat "out/data/${proof_hash}_blobs/$rule.json"                       \
+            | jq .term.att                                                  \
+            | grep --only-matching "label($proof_hash_upper.[A-Za-z.]*)"    \
+            | cut --delimiter='(' --field=2 | cut --delimiter=')' --field=1 \
+            | grep --only-matching '\..*'
+    done | sort -u
+}
+found_rules=($(find_rules))
 
 count_found_rules="${#found_rules[@]}"
 
